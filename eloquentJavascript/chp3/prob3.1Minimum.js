3757e9c642ca935198a921bc3b6d2111f415035d
AmadouSallah <amadousallah13@gmail.com>
2016-10-18 15:22:58 -0700

    Added problem statement for prob3.1Minimum.js

---
+++ eloquentJavascript/chp3/prob3.1Minimum.js
@@ -0,0 +1,14 @@
+/*
+Problem 3.1: Minimum
+
+The previous chapter introduced the standard function Math.min that returns its smallest argument.
+We can do that ourselves now. Write a function min that takes two arguments and returns their minimum.
+
+// Your code here.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+*/
