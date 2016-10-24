bb39ce0420727ce23432d2c1fe104bac4408504b
AmadouSallah <amadousallah13@gmail.com>
2016-10-24 16:13:00 -0700

    Added part 3 solution for prob4.1SumOfRange.js

--- eloquentJavascript/chp4/prob4.1SumOfRange.js
+++ eloquentJavascript/chp4/prob4.1SumOfRange.js
@@ -48,7 +48,21 @@ function sum(array) {
   return total;
 }
 
+// Part 3:
+function range(start, end, step) {
+  if (step == null) step = 1;
+  var array = [];
 
+  if (step > 0) {
+    for (var i = start; i <= end; i += step)
+      array.push(i);
+  } else {
+    for (var i = start; i >= end; i += step)
+      array.push(i);
+  }
+  return array;
+}
 
 console.log(range(1, 10)); // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(sum(range(1, 10))); // → 55
+console.log(range(5, 2, -1)); // → [5, 4, 3, 2]
